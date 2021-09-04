236640e54e77f077f19415d355b67af447e22454
Kashif-ali123 <89835659+Kashif-ali123@users.noreply.github.com>
2021-09-04 03:00:26 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Consulting from "./pages/Consulting";
+import Contact from "./pages/Contact";
+import Design from "./pages/Design";
+import Development from "./pages/Development";
+import Marketing from "./pages/Marketing";
+import Products from "./pages/Products";
+import More from "./pages/More";
+import Signup from "./pages/Signup";
+import Dash from "./pages/Dash";
+// import SignInOutContainer from './containers';
+// import Signup from './components/signup'
+import { GlobalStyles } from './global-styles';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css/animate.min.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/products" component={Products}></Route>
+          <Route path="/more" component={More}></Route>
+          <Route path="/dash" component={Dash}></Route>
+          <Route path="/signup" component={Signup}></Route>
+          <Route path="/marketing" component={Marketing}></Route>
+          <Route path="/development" component={Development}></Route>
+          <Route path="/design" component={Design}></Route>
+          <Route path="/consulting" component={Consulting}></Route>
+        </Switch>
+      </BrowserRouter>
+      {/* <SignInOutContainer /> */}
+    </>
+  );
+}
+
+export default App;
